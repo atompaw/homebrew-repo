3b1aa16791a0d91355712d42fa446c1babb7bcce
Marc Torrent <marc.torrent@cea.fr>
2019-08-12 17:06:38 +0200

    Create initial version of Formula (v4.1.0.6)
    truc

---
+++ Formula/atompaw.rb
@@ -0,0 +1,45 @@
+class Atompaw < Formula
+  desc "AtomPAW generates atomic datasets needed for performing electronic structure calculations based on the projector augmented-wave (PAW) method."
+  homepage "http://users.wfu.edu/natalie/papers/pwpaw"
+  url "http://users.wfu.edu/natalie/papers/pwpaw/atompaw-4.1.0.6.tar.gz"
+  sha256 "42a46c0569367c0b971fbc3dcaf5eaec7020bdff111022b6f320de9f11c41c2c"
+
+  #bottle do
+  #  root_url "http://forge.abinit.org/homebrew"
+  #  cellar :any
+  #  sha256 "xxx" => :mojave
+  #end
+
+  depends_on "gcc" if OS.mac? # for gfortran
+  depends_on "libxc" => :recommended
+  if OS.mac?
+    depends_on "veclibfort"
+  end
+
+  def install
+    #ENV.deparallelize
+    args = %W[--prefix=#{prefix}
+              --disable-shared]
+    args << "--with-linalg-incs=-I#{Formula["veclibfort"].opt_include}"
+    args << "--with-linalg-libs=-L#{Formula["veclibfort"].opt_lib} -lvecLibFort"
+
+    if build.with? "libxc"
+      args << "--enable-libxc"
+      args << "--with-libxc-incs=-I#{Formula["libxc"].opt_include}"
+      args << "--with-libxc-libs=-L#{Formula["libxc"].opt_lib} -lxc -lxcf90"
+    end
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    atompaw_tests = %W[#{doc}/example/F/lda/F.input
+                       #{doc}/example/Li/lda/Li.input
+                       #{doc}/example/Li/hf/Li.input]
+    atompaw_tests.each do |atompaw_input|
+      system "#{bin}/atompaw < #{atompaw_input} > atompaw.log"
+    end
+  end
+
+end
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# homebrew-atompaw
-Home-brew tap for AtomPAW
+# Homebrew tap for ATOMPAW
+
+This is a [Homebrew](https://brew.sh/) [tap](https://docs.brew.sh/Taps) for the [AtomPAW](https://www.abinit.org) code.
+
+## Installing
+
+Install Homebrew and run
+
+```
+brew tap atompaw/repo
+brew install atompaw
+```
