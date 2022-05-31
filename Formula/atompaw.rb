0d82e2aaa4c5f3698d04e7170702526a751334d0
Marc Torrent <marc.torrent@cea.fr>
2022-05-31 23:26:23 +0200

    Add arm64 monterey bottle

--- Formula/atompaw.rb
+++ Formula/atompaw.rb
@@ -6,12 +6,13 @@ class Atompaw < Formula
 
   bottle do
     root_url "http://forge.abinit.org/homebrew"
-    sha256 cellar: :any,                 monterey:      "7129bc21f1851443ea36655ffa83c49c82856b13b400b9e931982ff35c2821dd"
-    sha256 cellar: :any,                 big_sur:       "d06669982c920c5c2c1b50608921087c585ca0c239613ad6d2407633ef70b0a5"
-    sha256 cellar: :any,                 catalina:      "e83d13cd2b3b7cacd4c4a1600f36e13065ef0ea14b1f5bc47905715f9c423ce3"
-    sha256 cellar: :any,                 mojave:        "d3b68d05997bad9925c8927a5b43a8e14f4f1c43e72ae73aeec37e1b99ce796e"
-    sha256 cellar: :any,                 high_sierra:   "ec0979e8daf12ad4f587dea4c18d9a3b237f1430eec27d3faa1317364e6a33a3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "852b414bf11eab7c4b6ef34a047b77bd09a8d475b4094087348d1d672b30330f"
+    sha256 cellar: :any,                 arm64_monterey: "d9272b3c1d60ceff03b0ecb8681aefa31b6ba3a73527c51067b726c01c9875bf"
+    sha256 cellar: :any,                 monterey:       "7129bc21f1851443ea36655ffa83c49c82856b13b400b9e931982ff35c2821dd"
+    sha256 cellar: :any,                 big_sur:        "d06669982c920c5c2c1b50608921087c585ca0c239613ad6d2407633ef70b0a5"
+    sha256 cellar: :any,                 catalina:       "e83d13cd2b3b7cacd4c4a1600f36e13065ef0ea14b1f5bc47905715f9c423ce3"
+    sha256 cellar: :any,                 mojave:         "d3b68d05997bad9925c8927a5b43a8e14f4f1c43e72ae73aeec37e1b99ce796e"
+    sha256 cellar: :any,                 high_sierra:    "ec0979e8daf12ad4f587dea4c18d9a3b237f1430eec27d3faa1317364e6a33a3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "852b414bf11eab7c4b6ef34a047b77bd09a8d475b4094087348d1d672b30330f"
   end
 
   depends_on "gcc" if OS.mac? # for gfortran
