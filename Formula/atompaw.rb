48d5ebffd31eb4dc8bca72ab74b33d6a9b29523c
torrentm <torrentm@u-un00306576>
2022-11-03 14:02:26 +0100

    atompaw 4.2.0.2: add arm64 bottles

--- Formula/atompaw.rb
+++ Formula/atompaw.rb
@@ -6,7 +6,9 @@ class Atompaw < Formula
 
   bottle do
     root_url "http://forge.abinit.org/homebrew"
-    # sha256 cellar: :any, arm64_monterey: ""
+    sha256 cellar: :any, arm64_ventura: "dd163b4c81ef91059e3ff4d49ed7541fd932656f8ee9c7d85c669aa5e827067a"
+    sha256 cellar: :any, arm64_monterey: "55b56211f19afdd2ee71a3fb2fe11e74aca0035d8d22b1726ff7bb171d539919"
+    sha256 cellar: :any, ventura: "1f49678812a8bcca0bfb58b725588945c07ca17ce0b6d4104d75c33ffaa32450"
     sha256 cellar: :any, monterey: "8faad12a405dfe7209b102b6d1e37e68bff8732fc473190e938ffe0e7a5d4629"
     sha256 cellar: :any, big_sur:  "c1a99a6139c67de4209ed9ec928fcf214fd3a7d9acd22c9373f3ddaf69ed8428"
     sha256 cellar: :any, catalina: "1b92fe1aef0849d371d0b78d225d4cb219a8179ce9334a542df760992558f394"
